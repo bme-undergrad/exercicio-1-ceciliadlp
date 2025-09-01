9c20df85c7cb0fe9bde7178926d12fa3c3f73654
ceciliadlp <cecideluca13@gmail.com>
2025-09-01 15:05:14 -0300

    resolve erro do loop

--- exercicio1.m
+++ exercicio1.m
@@ -7,8 +7,19 @@ imax = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
-t = 0; % alterar
+t= zeros(imax,1);
+t(1)= x0;
+
+for j =1:length(t)-1
+  if j ~=1
+    erro(j)=abs((t(j)-t(j-1))/t(j))
+    if erro(j)<es
+      break
+    endif
+  endif
+  t(j+1)=t(j)- (func(t(j)/func_d(t(j))));
+endfor
+t=t(j)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
